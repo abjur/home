ebce726fece3a25165c2a037ddd48c6e60d3a82b
Julio Trecenti <julio.trecenti@gmail.com>
2017-01-12 18:04:42 -0200

    some tweaks

--- _deploy.sh
+++ _deploy.sh
@@ -8,10 +8,10 @@ set -e
 git config --global user.email "j@abj.org.br"
 git config --global user.name "ABJ"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git curso-output
-cd curso-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git site-output
+cd site-output
 git rm -rf .
 cp -r ../public/* ./
 git add --all *
-git commit -m "Update curso-r" || true
+git commit -m "Update Site ABJ" || true
 git push origin gh-pages
