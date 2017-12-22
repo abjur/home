698c879c1b60ca38aebbefcea661cfb54cfbfbd1
ABJ <j@abj.org.br>
2017-12-22 13:41:33 +0000

    Update Site ABJ

--- public/js/global.js
+++
@@ -1,9 +0,0 @@
-var intros = document.querySelectorAll('.intro');
-Array.prototype.forEach.call(intros, function (intro) {
-  $clamp(intro, {clamp: 4});
-});
-
-var titles = document.querySelectorAll('.box-image-text .content h4 a');
-Array.prototype.forEach.call(titles, function (title) {
-  $clamp(title, {clamp: 2});
-});
